faa948f9d8baeb8ff03519e150b945146e0f7d1e
francoiseee <150410739+francoiseee@users.noreply.github.com>
2023-12-19 22:26:40 +0800

    Update medjCalcu.java

--- medjCalcu.java
+++ medjCalcu.java
@@ -12,6 +12,29 @@ class medjCalcu{
 		int c = a+b;
 
 		System.out.println("The total of the first and second number: "+ c);
+
+
+/* import java.util.Scanner;
+
+public class MedjCalcu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+
+        int c = a + b;
+
+        System.out.println("The total of the first and second number: " + c);
+
+        scanner.close(); // Remember to close the scanner when done using it.
+*/
+    }
+}
+
 	}
 
 }
