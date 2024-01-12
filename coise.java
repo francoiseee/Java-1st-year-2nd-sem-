7186a25c2c531f6c6cc426384ba81abe2959702d
francoiseee <150410739+francoiseee@users.noreply.github.com>
2024-01-12 22:27:20 +0800

    Create coise.java

---
+++ coise.java
@@ -0,0 +1,14 @@
+class coise {
+    public static void main(String[] args) {
+
+        int i;
+        final int dulo = 11;
+
+        i = 1;
+        while (i < dulo)
+        {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+}
